89588c228de6a59b0993f77a5eef4422417ff7c2
dehvun-loki <dlocke9@gmail.com>
2015-12-08 18:10:10 -0500

    creating a cube by script and attach camera

---
+++ EvolutionRevolution/Assets/Scripts/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(player, new Vector3(0, 0.5f, 0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

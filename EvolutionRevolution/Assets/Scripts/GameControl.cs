3ebcf52d6e30dbeae51d9b39060cc64833c64529
dehvun-loki <dlocke9@gmail.com>
2015-12-08 19:19:04 -0500

    added persistence basics

---
+++ EvolutionRevolution/Assets/Scripts/GameControl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl control;
+
+    public float health;
+    public float exp;
+
+
+	void Awake () {
+        if(control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if(control != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerinfo.dat");
+
+        PlayerData data = new PlayerData();
+        data.health = health;
+        data.exp = exp;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/playerinfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerinfo.dat", FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+            health = data.health;
+            exp = data.exp;
+        }
+    }
+}
+
+[Serializable]
+class PlayerData
+{
+    public float health;
+    public float exp;
+    //should do gets and sets and constructors
+}
--- EvolutionRevolution/Assets/Scripts/StartGame.cs
+++ EvolutionRevolution/Assets/Scripts/StartGame.cs
@@ -1,17 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StartGame : MonoBehaviour {
+public class StartGame : MonoBehaviour
+{
 
     public Transform player;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         Instantiate(player, new Vector3(0, 0.5f, 0), Quaternion.identity);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
 }
